8c9eef3d9a17048056ee7e501054527a34d80d5a
MasatoshiMizumoto <51846213+MasatoshiMizumoto@users.noreply.github.com>
2023-01-06 01:26:40 +0900

    create basic method

---
+++ src/main/java/com/raisetech/springbootstudy/CreateForm.java
@@ -0,0 +1,11 @@
+package com.raisetech.springbootstudy;
+
+public class CreateForm {
+  private String name;
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+}
