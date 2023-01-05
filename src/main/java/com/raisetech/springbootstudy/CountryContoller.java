b1f2de1b911cc69de6e692129d5b706ba7cf018b
MasatoshiMizumoto <51846213+MasatoshiMizumoto@users.noreply.github.com>
2023-01-05 17:22:51 +0900

    add CountryContoller

---
+++ src/main/java/com/raisetech/springbootstudy/CountryContoller.java
@@ -0,0 +1,14 @@
+package com.raisetech.springbootstudy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryContoller {
+
+  @GetMapping("/countries/{country}")
+  public String countries(@PathVariable String country) {
+    return "Hello " + country.substring(0, 1).toUpperCase() + country.substring(1) + "!";
+  }
+  }
